f05e60bb7aaa56ebf1d04f63108315ed004e99b4
Yeonjun Lee <yeonfish6040@gmail.com>
2024-11-15 10:51:31 +0900

    feat: permission management controller

--- src/routes/user/controllers/user.controller.ts
+++ src/routes/user/controllers/user.controller.ts
@@ -1,7 +1,13 @@
 import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
-import { CreateUserDTO } from "../dto";
+import { CustomJwtAuthGuard } from "../../../auth/guards";
+import {
+  AddPermissionDTO,
+  CreateUserDTO,
+  RemovePermissionDTO,
+  SetPermissionDTO,
+} from "../dto";
 import { UserService } from "../providers";
 
 @ApiTags("User Manage")
@@ -17,5 +23,37 @@ export class UserController {
   @Post("/register")
   async register(@Body() data: CreateUserDTO) {
     return await this.userManageService.createUser(data);
+  }
+
+  // TODO: permission set
+
+  @ApiOperation({
+    summary: "권한 설정",
+    description: "유저 권한 설정",
+  })
+  @UseGuards(CustomJwtAuthGuard)
+  @Post("/permission/set")
+  async setPermission(@Body() data: SetPermissionDTO) {
+    return await this.userManageService.setPermission(data);
+  }
+
+  @ApiOperation({
+    summary: "권한 추가",
+    description: "유저 권한 추가",
+  })
+  @UseGuards(CustomJwtAuthGuard)
+  @Post("/permission/add")
+  async addPermission(@Body() data: AddPermissionDTO) {
+    return await this.userManageService.addPermission(data);
+  }
+
+  @ApiOperation({
+    summary: "권한 제거",
+    description: "유저 권한 제거",
+  })
+  @UseGuards(CustomJwtAuthGuard)
+  @Post("/permission/remove")
+  async removePermission(@Body() data: RemovePermissionDTO) {
+    return await this.userManageService.removePermission(data);
   }
 }
