a626067ec6aa7900b8f33a7a2a4f563697762032
Yeonjun Lee <yeonfish6040@gmail.com>
2024-11-29 12:01:40 +0900

    limit: payload

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { json } from "express";
 
 import { AppModule } from "./app";
 import { CustomSwaggerSetup } from "./common/modules/swagger.module";
@@ -6,6 +7,7 @@ import { ValidationService } from "./common/modules/validation.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: "5000mb" }));
   app.enableCors();
 
   await CustomSwaggerSetup(app);
