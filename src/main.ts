0c1f3e7106c8e1de5d8c8532601499e75aaa11a5
Yeonjun Lee <yeonfish6040@gmail.com>
2024-12-06 22:53:54 +0900

    sign test

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ import { ValidationService } from "./common/modules/validation.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
 
   app.enableCors();
