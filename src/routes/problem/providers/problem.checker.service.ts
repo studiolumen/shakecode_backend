19d9e50a64fdcd790f43bc2b2fbc151226f06eda
Yeonjun Lee <yeonfish6040@gmail.com>
2024-12-18 00:25:26 +0900

    chore. this is the line

--- src/routes/problem/providers/problem.checker.service.ts
+++ src/routes/problem/providers/problem.checker.service.ts
@@ -7,12 +7,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-import {
-  CompilerType,
-  MatchRoomElement,
-  TestCodeResult,
-  UserJWT,
-} from "../../../common/mapper/types";
+import { CompilerType, TestCodeResult } from "../../../common/mapper/types";
 import { Problem, Testcase } from "../../../schemas";
 
 @Injectable()
